8ec3b37d562f088ee55145171fac250d854df476
Test <test@test.com>
2026-05-19 08:55:20 +0700

    commit

--- api/index.js
+++ api/index.js
@@ -347,4 +347,6 @@ app.get('/api/stats/jurusan', async (req, res) => {
 
 initDb().catch(console.error);
 
-export default app;
+export default async function handler(req, res) {
+  return app(req, res);
+}
